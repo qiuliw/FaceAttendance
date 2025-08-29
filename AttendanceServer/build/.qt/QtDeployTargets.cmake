1870502bb7bc117ba41ac21267f8c02d84308867
qiuliw <a1807191473@gmail.com>
2025-08-29 19:36:01 +0800

    创建数据库2

--- AttendanceServer/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AttendanceServer_FILE D:/Github/FaceAttendance_Qt/AttendanceServer/build/AttendanceServer.exe)
-set(__QT_DEPLOY_TARGET_AttendanceServer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AttendanceServer_RUNTIME_DLLS D:/Software/Qt/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Software/Qt/6.9.1/msvc2022_64/bin/Qt6Networkd.dll;D:/Software/Qt/6.9.1/msvc2022_64/bin/Qt6Sqld.dll;D:/Software/Qt/6.9.1/msvc2022_64/bin/Qt6Guid.dll;D:/Software/Qt/6.9.1/msvc2022_64/bin/Qt6Cored.dll)
